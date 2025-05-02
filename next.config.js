a37a61a8be8ed8a6d60645955e74beebc5af9b42
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 20:28:39 +0300

    Update next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.m?js/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
