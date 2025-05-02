757f18c05dfb6f024ea68924d418f7d327d461e7
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 23:31:46 +0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -20,4 +20,4 @@ export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const functions = getFunctions(app);
+export const functions = getFunctions(app); 
