06009daa979c124ed62f795ab3f7cbf4a78700d0
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 22:55:34 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import App from './app/layout';
+import App from './app/layout'; // Assumes routes are handled inside App
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
