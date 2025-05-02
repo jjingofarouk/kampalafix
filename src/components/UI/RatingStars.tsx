dd66d543f80ca2f0b5b01887d20f6d18ab436da8
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 01:06:46 +0300

    Update RatingStars.tsx

--- src/components/UI/RatingStars.tsx
+++ src/components/UI/RatingStars.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { FaStar } from 'react-icons/fa';
+
 interface RatingStarsProps {
   rating: number;
 }
 
 export default function RatingStars({ rating }: RatingStarsProps) {
   return (
-    <div className="flex">
+    <div className="flex space-x-1">
       {[...Array(5)].map((_, i) => (
-        <span key={i} className={i < rating ? 'text-yellow-400' : 'text-gray-300'}>
-          ★
-        </span>
+        <FaStar
+          key={i}
+          className={`text-2xl transition-all duration-300 ${
+            i < rating
+              ? 'text-yellow-400 drop-shadow-[0_0_8px_rgba(234,179,8,0.6)] hover:scale-125'
+              : 'text-gray-600/50 hover:text-gray-500/80'
+          }`}
+        />
       ))}
     </div>
   );
