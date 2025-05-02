dbd28542cc7097321ca52d4d090dcafc09846a20
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 17:03:27 +0300

    Update chatSlice.ts

---
+++ src/store/chatSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { ChatMessage } from '../types/chat';
+
+interface ChatState {
+  messages: ChatMessage[];
+}
+
+const initialState: ChatState = {
+  messages: [],
+};
+
+const chatSlice = createSlice({
+  name: 'chat',
+  initialState,
+  reducers: {
+    addMessage(state, action) {
+      state.messages.push(action.payload);
+    },
+    clearMessages(state) {
+      state.messages = [];
+    },
+  },
+});
+
+export const { addMessage, clearMessages } = chatSlice.actions;
+export default chatSlice.reducer;
