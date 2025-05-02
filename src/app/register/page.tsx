94d61ed1e5ddb2115d6a4b3023b795cce8ab7dde
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 00:19:18 +0300

    Update page.tsx

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+// app/register/page.tsx
+import RegisterForm from '../../components/Auth/RegisterForm';
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <RegisterForm />
+    </div>
+  );
+}
