7898ce96111bc2e2974a737601f518c69ccba2d4
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 17:02:26 +0300

    Update authSlice.ts

---
+++ src/store/authSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { User } from '../types/user';
+
+interface AuthState {
+  user: User | null;
+  isAuthenticated: boolean;
+}
+
+const initialState: AuthState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload;
+      state.isAuthenticated = !!action.payload;
+    },
+    clearUser(state) {
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
