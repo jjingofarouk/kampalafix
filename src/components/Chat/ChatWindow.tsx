711cd212192d8139d2932efbbd86aec0d7a6d5a2
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 16:30:19 +0300

    Update ChatWindow.tsx

---
+++ src/components/Chat/ChatWindow.tsx
@@ -0,0 +1,26 @@
+import { useChat } from '../../hooks/useChat';
+import MessageBubble from './MessageBubble';
+import TypingIndicator from './TypingIndicator';
+
+export default function ChatWindow() {
+  const { messages, sendMessage } = useChat();
+
+  const handleSend = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle send message
+  };
+
+  return (
+    <div className="flex flex-col h-[500px] bg-white rounded-lg shadow">
+      <div className="flex-1 overflow-y-auto p-4">
+        {messages.map((msg) => (
+          <MessageBubble key={msg.id} message={msg} />
+        ))}
+      </div>
+      <TypingIndicator />
+      <form onSubmit={handleSend} className="p-4">
+        <input type="text" className="w-full p-2 border rounded" placeholder="Type a message..." />
+      </form>
+    </div>
+  );
+}
