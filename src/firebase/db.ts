41ed9b28b488dff4bdd19e27e5da0bb4314384b7
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 21:38:13 +0300

    Update db.ts

--- src/firebase/db.ts
+++ src/firebase/db.ts
@@ -1,11 +1,15 @@
 import { db } from './config';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { collection, addDoc, getDocs, DocumentData } from 'firebase/firestore';
+import { Booking } from '../types/booking';
 
-export const addDocument = async (collectionName: string, data: any) => {
+export const addDocument = async (collectionName: string, data: DocumentData) => {
   return await addDoc(collection(db, collectionName), data);
 };
 
-export const getDocuments = async (collectionName: string) => {
+export const getDocuments = async (collectionName: string): Promise<Booking[]> => {
   const snapshot = await getDocs(collection(db, collectionName));
-  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  } as Booking));
 };
