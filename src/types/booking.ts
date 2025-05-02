cfac0475730e3ac591d09417a4149c10ced0d09d
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 00:42:40 +0300

    Update booking.ts

--- src/types/booking.ts
+++ src/types/booking.ts
@@ -2,6 +2,7 @@ export interface Booking {
   id: string;
   serviceId: string;
   serviceName: string;
+className?: string;
   date: string; // or Date, depending on your Firestore data
   status: 'pending' | 'confirmed' | 'cancelled';
 }
