abf54b47b7a1cf07bb6342a0f02433ccbd05b34a
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 20:53:09 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    webpackBuildWorker: true, // Ensure Webpack build worker is enabled
+  },
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.m?js/,
-      resolve: {
-        fullySpecified: false,
+      test: /\.(m?js|cjs)$/,
+      include: [/node_modules\/undici/], // Explicitly target undici
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          cacheDirectory: true,
+        },
       },
     });
+    config.resolve.fullySpecified = false;
     return config;
   },
 };
