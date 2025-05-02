424cd7c6359b346f6026d60155f592c5d7f6722c
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 23:17:37 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,5 @@
-// app/layout.tsx
-
-"use client";
-
 import '../styles/globals.css';
-import { ThemeProvider } from '../context/ThemeContext';
-import Navbar from '../components/Layout/Navbar';
-import Footer from '../components/Layout/Footer';
-import { Provider } from 'react-redux';
-import { store } from '../store';
+import ClientLayout from './ClientLayout';
 
 export const metadata = {
   title: 'KampalaFix',
@@ -18,13 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <Provider store={store}>
-          <ThemeProvider>
-            <Navbar />
-            <main>{children}</main>
-            <Footer />
-          </ThemeProvider>
-        </Provider>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
