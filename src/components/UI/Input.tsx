704a353b78faadd55414481b098bf2760c61683b
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-02 16:19:43 +0300

    Update Input.tsx

---
+++ src/components/UI/Input.tsx
@@ -0,0 +1,10 @@
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export default function Input({ ...props }: InputProps) {
+  return (
+    <input
+      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+      {...props}
+    />
+  );
+}
